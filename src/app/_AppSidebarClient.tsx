3b8073a8daea66f70909c4098a4bcc55daa72590
Emi Legaspi <emilanylegaspi@gmail.com>
2025-09-05 13:47:55 +1200

    setup sidebar with shadcn

---
+++ src/app/_AppSidebarClient.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { SidebarTrigger } from '@/components/ui/sidebar'
+import { useIsMobile } from '@/hooks/use-mobile'
+
+export function AppSidebarClient({ children }: { children: React.ReactNode }) {
+  const isMobile = useIsMobile()
+
+  if (isMobile)
+    return (
+      <div className="flex flex-col w-full">
+        <div className="p-2 border-b flex items-center gap-1">
+          <SidebarTrigger />
+          <span className="text-xl">Job Board</span>
+        </div>
+        <div className="flex-1 flex">{children}</div>
+      </div>
+    )
+
+  return children
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,39 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar'
+import { AppSidebarClient } from './_AppSidebarClient'
+
 export default function Home() {
-  return <h1>HI</h1>
+  return (
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">Job Board</span>
+          </SidebarHeader>
+
+          <SidebarContent></SidebarContent>
+
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>Button</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+
+        <main className="flex-1"></main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  )
 }
